edae4f5e53c7c8f71f4e03ffd2f1228ab20f7d6a
jinyung2 <jinyoungchoi118@gmail.com>
2020-10-08 07:16:55 -0700

    performed some filtering operatins, outputed state-city.json

--- json-trim.js
+++ json-trim.js
@@ -1,16 +1,80 @@
-const { raw } = require('express');
 const fs = require('fs');
+const ndjson = require('ndjson');
 
-let rawdata = fs.readFileSync('/home/jin/Downloads/inventory.crud.json');
-let trimmed = rawdata.toString().split('\n')
-.filter(entry => entry != "")
-.map(entry => {
-    jsonEntry = JSON.parse(entry);
-    delete jsonEntry.item;
-    delete jsonEntry.qty;
-    return jsonEntry;
+// YELP JSON FILE TRIM SCRIPT
+
+// some different consts for files
+const TEST_FILE = 'inventory.crud.json'
+const INPUT_FILE = '../yelp_academic_dataset_business.json';
+const OUTPUT_FILE = 'yelp_business_trimmed.json'
+
+const stateCityDict = {};
+
+let container = [];
+
+const streamPromise = new Promise((res, rej) => {
+    fs.createReadStream(INPUT_FILE)
+        .pipe(ndjson.parse())
+        .on('data', (data) => {
+            if (!stateCityDict[data.state]) {
+                stateCityDict[data.state] = [data.city];
+            } else {
+                if (!stateCityDict[data.state].find(e => e == data.city)) {
+                    stateCityDict[data.state].push(data.city);
+                }
+            }
+        })
+        .on('end', () => {
+            res();
+        });
+    });
+        
+
+    // fs.createReadStream(INPUT_FILE)
+    //     .pipe(ndjson.parse())
+    //     .on('data', (data) => {
+    //         return new Promise((res, rej) => {
+    //             res(data);
+    //         }).then((data) => {
+    //             state.add(data.state);
+    //             city.add(data.city);
+    //         })
+
+                // delete fields here
+
+                //
+                // return new Promise((res, rej) => {
+                //     res(JSON.stringify(data) + '\n');
+                // })
+                // .then(() => {
+
+                // }) 
+                // .then((data) => {
+                //     fs.appendFile(OUTPUT_FILE, data, (err) => {
+                //         if (err) {
+                //             throw err;
+                //         }
+                //         console.log('data appended');
+                //     });
+                // })
+        //         .catch(err => { console.log(err) });
+        // })
+
+// streamPromise.then(() => {
+//     fs.createWriteStream('test.json')
+// }).then((data) => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+streamPromise.then(() => {
+    const data = JSON.stringify(stateCityDict, null, 2);
+    fs.writeFile('state-city.json', data, (err) => {
+        if (err) throw err;
+        console.log("successfully wrote file");
+    })
+}).catch(err => {
+    console.log('lol error');
 })
-
-let data = JSON.stringify({...trimmed});
-console.log(data);
-fs.writeFileSync('inventory-trimmed.json', data);
+// fs.writeFileSync()
