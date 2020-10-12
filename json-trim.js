6a98f1fcff8fde08176d2c28a1c2bab07db1602b
jinyung2 <jinyoungchoi118@gmail.com>
2020-10-11 17:53:24 -0700

    committed script for trimming, gitignored actual trimmed files

--- json-trim.js
+++ json-trim.js
@@ -3,42 +3,154 @@ const ndjson = require('ndjson');
 
 // YELP JSON FILE TRIM SCRIPT
 
+// World's worst file and function organization!
+// But it works!
+
 // some different consts for files
 const TEST_FILE = 'inventory.crud.json';
 const INPUT_FILE = '../yelp_academic_dataset_business.json';
 const OUTPUT_FILE = 'yelp_business_trimmed.json';
 
+const BUSINESS_INPUT = 'yelp_business_trimmed.json';
+const BUSINESS_OUTPUT = 'business_id_map.json';
+
+const BUSINESS_IDS = 'business_id_map.json';
+
+const CHECKIN_INPUT = '../yelp_academic_dataset_checkin.json';
+const CHECKIN_OUTPUT = 'yelp_checkin_trimmed_2.json';
+
+const REVIEW_INPUT = '../yelp_academic_dataset_review.json';
+const REVIEW_OUTPUT = 'yelp_review_trimmed_2.json';
+
+const TIP_INPUT = '../yelp_academic_dataset_tip.json';
+const TIP_OUTPUT = 'yelp_tip_trimmed.json';
+
+const PHOTOS_INPUT = '../photos.json';
+const PHOTOS_OUTPUT = 'yelp_photos_trimmed.json';
+
+
 // create function that represents stream
 // one line of data comes in, checks if it is Las Vegas, NV or Toronrto, ON
 // if it is gets passed through and trimmed (remove is_open, attributes.BusinessParking) 
 // written to the new output json
-fs.createReadStream(INPUT_FILE)
-.pipe(ndjson.parse())
-.on('data', (data) => {
-    return new Promise((res, rej) => {
-        if (data?.attributes?.RestaurantsPriceRange2) {
-            data.priceRange = data.attributes.RestaurantsPriceRange2;
-            delete data.is_open;
-            delete data.attributes;
-            res(data);
-        }
-    }).then((trimmedData) => { // data with is_open removed and pricerange extracted into separate property
-        if (!(trimmedData.state == "NV" && trimmedData.city == "Las Vegas") &&
-            !(trimmedData.state == "ON" && trimmedData.city == "Toronto")) {
-                delete trimmedData;
-                return;
-            } 
+
+
+const writeToFile = (data, filename) => {
+    if (!data) {
+        return;
+    }
+    fs.appendFile(filename, data + '\n', (err) => {
+        if (err) throw err;
+    });
+};
+
+// enter string of "city, state"
+const filterStateCity = (trimmedData, ...cityState) => {
+    for (let i = 0; i < cityState.length; i++) {
+        if (trimmedData.state == cityState[i][1] && trimmedData.city == cityState[i][0]) {
             return trimmedData;
-            
-    })
-    .then((filteredData) => { // Toronto and Las Vegas
-            return JSON.stringify(filteredData);
-    }).then((data) => {
-        if (!data) {
-            return;
         }
-        fs.appendFile(OUTPUT_FILE, data + '\n', (err) => {
-            if (err) throw err;
+    }
+    delete trimmedData;
+    return;
+    // if (!(cityState.includes(trimmedData.state) && cities.includes()) &&
+    // !(trimmedData.state == "ON" && trimmedData.city == "Toronto")) {
+    //     delete trimmedData;
+    //     return;
+    // }
+    // return trimmedData;
+
+}
+
+//
+const trimBusiness = (filename) => fs.createReadStream(filename)
+    .pipe(ndjson.parse())
+    .on('data', (data) => {
+        return new Promise((res, rej) => {
+            if (data?.attributes?.RestaurantsPriceRange2) {
+                data.priceRange = data.attributes.RestaurantsPriceRange2;
+                delete data.is_open;
+                delete data.attributes;
+                res(data);
+            }
+        }).then((data) => {
+            return filterStateCity(data, ["Toronto", "ON"], ["Las Vegas", "NV"]);
         })
+            .then((filteredData) => { // Toronto and Las Vegas
+                return JSON.stringify(filteredData);
+            }).then(data => { writeToFile(data, OUTPUT_FILE) });
+    });
+
+
+// sort through trimmed business json and collect all the business ids
+// make this into a map for faster retrieval!
+
+const trimByBusinessId = (input, output) => {
+    let container = new Map;
+    fs.createReadStream(input)
+        .pipe(ndjson.parse())
+        .on('data', (data) => {
+            let businessId = data.business_id;
+            container.set(businessId, true);
+        })
+        .on('end', () => {
+            let data = JSON.stringify([...container]);
+            // console.log("data: " + data);
+            fs.writeFile(output, data, (err) => {
+                if (err) throw err;
+            })
+        })
+}
+
+// 
+const businessFilter = (input, idList, output, selectedFunc) => {
+    let businessIds = new Map(JSON.parse(fs.readFileSync(idList)));
+    fs.createReadStream(input)
+    .pipe(ndjson.parse())
+    .on('data', (data) => {
+        return new Promise((res, rej) => {
+            if (businessIds.get(data.business_id)) {
+                res(data);
+            }
+        }).then((data) => selectedFunc(data)
+        ).then((trimmedData) => {
+            writeToFile(trimmedData, output);
+        })
+        .catch((err) => {
+            console.log("not found");
+        });
+    }).on('end', () => {
+        console.log("Finished trimming!");
     })
-});
+
+}
+
+const checkInFilter = (data) => {
+    const numCheckins = data.date.split(', ').length;
+    data.checkin_count = numCheckins;
+    delete data.date;
+    return JSON.stringify(data);   
+}
+
+const reviewFilter = (data) => {
+    delete data.user_id;
+    return JSON.stringify(data);
+}
+
+const tipFilter = (data) => {
+    delete data.date;
+    delete data.user_id;
+    return JSON.stringify(data);
+}
+
+const photosFilter = (data) => {
+    return JSON.stringify(data);
+}
+
+
+// trimByBusinessId(BUSINESS_INPUT, BUSINESS_OUTPUT);
+// businessFilter(CHECKIN_INPUT, BUSINESS_IDS, CHECKIN_OUTPUT, checkInFilter);
+// businessFilter(REVIEW_INPUT, BUSINESS_IDS, REVIEW_OUTPUT, reviewFilter);
+// businessFilter(TIP_INPUT, BUSINESS_IDS, TIP_OUTPUT, tipFilter);
+// businessFilter(PHOTOS_INPUT, BUSINESS_IDS, PHOTOS_OUTPUT, photosFilter);
+
--- unlink.js
+++ unlink.js
@@ -1,5 +1,67 @@
 const fs = require('fs');
+const path = require('path');
+const ndjson = require('ndjson');
+const readline = require('readline');
+
+const PHOTOS_INPUT = 'yelp_photos_trimmed.json';
 
 // the unlink will be used to delete the photos from my file 
 // system that I do not need. this will be filtered via list of business ID.
-fs.unlink('test.txt', (err) => { console.log('deleted')});
+
+
+// PSEUDOCODE //
+// use list.txt file -> convert into a map
+// go through photos_input and remove the elements of map that exist (photos I need to keep)
+// iterate through the map of remaining photo ids and use unlink to delete
+
+let photoList = new Map();
+
+const rl = readline.createInterface({
+    input: fs.createReadStream("../list"),
+    crlfDelay: Infinity
+});
+const unlinkPhotos = () => rl.on('line', (line) => {
+    photoList.set(line, true);
+}).on('close', () => {
+    // done with creating map, filter out the ones from photo_id of PHOTOS_INPUT
+    fs.createReadStream(PHOTOS_INPUT).pipe(ndjson.parse())
+        .on('data', (data) => {
+            const photosPath = path.join('.', data.photo_id + '.jpg');
+            if (photoList.get("./" + photosPath)) {
+                photoList.delete("./" + photosPath);
+            }
+        }).on('error', (err) => {
+            throw err;
+        }).on('end', () => {
+            console.log(photoList.size);
+            // photoList has stuff that needs to be unlinked, iterate and unlink
+            for (keys of photoList.keys()) {
+                // keys = keys.substr(2);
+                // console.log(keys);
+                const deletePath = path.join('..','photos', keys);
+                fs.unlink(deletePath, (err) => {
+                    if (err) console.log("not found");
+                    console.log(deletePath, "was deleted.");
+                })
+            }
+        })
+});
+
+
+
+// const rl = readline.createInterface({
+//     input: fs.createReadStream(idList),
+//     crlfDelay: Infinity
+// });
+
+// fs.createReadStream(PHOTOS_INPUT).pipe(ndjson.parse())
+//     .on('data', (data) => {
+//         const photosPath = path.join('..', 'photos', data.photo_id + '.jpg');
+//         console.log(photosPath);
+//         // fs.unlink()
+//     }).on('error', (err) => {
+//         throw err;
+//     })
+
+// fs.unlink('test.txt', (err) => { console.log('deleted')});
+
