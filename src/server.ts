4f7ba01172fe8bf4f9d86e2a8605bb2912ff904d
jinyung2 <jinyoungchoi118@gmail.com>
2020-10-17 13:52:33 -0700

    changed port to 3000

--- src/server.ts
+++ src/server.ts
@@ -44,7 +44,7 @@ app.use((error: any, req: Request, res: Response, next: NextFunction) => {
 })
 
 mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log("Connected to server, listening on 8080")
+    console.log("Connected to server, listening on 3000")
     app.listen(3000);
 }).catch(err => {
     console.log(err);
